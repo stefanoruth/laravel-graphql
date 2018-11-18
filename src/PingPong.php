03dc91a6a03fcd809769307ddd5f3da837f21b15
Stefano Ruth <stefano6262@gmail.com>
2018-11-18 16:15:06 +0100

    PingPong

--- src/PingPong.php
+++ src/PingPong.php
@@ -5,8 +5,10 @@ namespace Ruth\GraphQL;
 use Ruth\GraphQL\Base\Query;
 use GraphQL\Type\Definition\Type;
 
-class Ping extends Query
+class PingPong extends Query
 {
+    public $name = 'ping';
+
     public function fields()
     {
         return Type::string();
